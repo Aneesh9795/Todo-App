03615d8c329f62a655ff0edc018d97879be94f89
Aneesh Patel <158780982+Aneesh9795@users.noreply.github.com>
2025-06-19 10:48:14 +0530

    task card home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,68 +1,54 @@
 import 'package:flutter/material.dart';
-
+import 'package:todoapp/widgets/task_card.dart';
 import 'add_task_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<List<dynamic>> todoList = [
+    ['Buy groceries', false],
+    ['Study Flutter', false],
+    ['Call Mom', false],
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      todoList[index][1] = value!;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF1E1E2C),
+      backgroundColor: Colors.deepPurple[200],
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Text(
-          'Todo App',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
+        title: const Text("TODO"),
+        backgroundColor: Colors.deepPurple[400],
+        elevation: 10,
       ),
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: List.generate(8, (index) {
-          return Container(
-            margin: EdgeInsets.only(bottom: 16),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.deepPurpleAccent.withOpacity(0.3),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.check_circle_outline, color: Colors.white),
-                SizedBox(width: 16),
-                Expanded(
-                  child: Text(
-                    'Task ${index + 1}',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                ),
-                Icon(Icons.arrow_forward_ios, color: Colors.white),
-              ],
-            ),
-          );
-        }),
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.deepPurpleAccent,
-        onPressed: () async {
-          final result = await Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddTaskScreen()),
-          );
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
+          itemCount: todoList.length,
+          separatorBuilder: (context, index) => SizedBox(height: 10), // yeh gap hai
+          itemBuilder: (context, index) {
+            return TaskCard(
+              isChecked: todoList[index][1],
+              onChanged: (value) {
+                setState(() {
+                  todoList[index][1] = value!;
+                });
+              },
+              todoText: todoList[index][0],
+            );
+          },
+        )
 
-          if (result == 'refresh') {
-
-            print("Refresh home screen tasks");
-          }
-
-        },
-        child: Icon(Icons.add, color: Colors.white),
       ),
     );
   }
--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -1,107 +1,40 @@
 import 'package:flutter/material.dart';
-import '../models/task_model.dart'; // Make sure to import the TaskModel
 
 class TaskCard extends StatelessWidget {
-  final TaskModel task;
-  final VoidCallback? onTap;
-  final VoidCallback? onStatusToggle;
+  final bool isChecked;
+  final String todoText;
+
+  final Function(bool?)? onChanged;
 
   const TaskCard({
     super.key,
-    required this.task,
-    this.onTap,
-    this.onStatusToggle,
+    required this.isChecked,
+    required this.todoText,
+    this.onChanged,
   });
-
-  Color getPriorityColor(String priority) {
-    switch (priority.toLowerCase()) {
-      case 'high':
-        return Colors.redAccent;
-      case 'medium':
-        return Colors.orangeAccent;
-      case 'low':
-        return Colors.greenAccent;
-      default:
-        return Colors.grey;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 8),
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          color: const Color(0xFF1E1E1E),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          children: [
-            // Status Checkbox or Icon
-            GestureDetector(
-              onTap: onStatusToggle,
-              child: Icon(
-                task.isCompleted
-                    ? Icons.check_circle
-                    : Icons.radio_button_unchecked,
-                color: task.isCompleted ? Colors.greenAccent : Colors.grey,
-                size: 28,
-              ),
+    return Container(
+      padding: const EdgeInsets.all(24),
+      decoration: BoxDecoration(
+        color: Colors.deepPurple[500],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Row(
+        children: [
+          Checkbox(value: isChecked, onChanged: onChanged),
+          Text(
+            'Todo',
+            style: TextStyle(
+              decoration:
+                  isChecked ? TextDecoration.lineThrough : TextDecoration.none,
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(width: 16),
-
-            // Task Info
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    task.title,
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: task.isCompleted
-                          ? Colors.grey
-                          : Colors.white,
-                      decoration: task.isCompleted
-                          ? TextDecoration.lineThrough
-                          : TextDecoration.none,
-                    ),
-                  ),
-                  const SizedBox(height: 6),
-                  Row(
-                    children: [
-                      Icon(Icons.access_time, color: Colors.grey, size: 16),
-                      const SizedBox(width: 4),
-                      Text(
-                        '${task.time.format(context)} | ${task.date.day}/${task.date.month}/${task.date.year}',
-                        style: const TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-
-            // Priority Badge
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-              decoration: BoxDecoration(
-                color: getPriorityColor(task.priority),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Text(
-                task.priority,
-                style: const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
